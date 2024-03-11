6581fd3c1f50414c4bde2c342ad3c4f6a895e6fb
Ahmad Mario <riotheassassin@gmail.com>
2024-03-11 14:41:11 +0700

    adding bootstrap

--- Latihan3/Form.php
+++ Latihan3/Form.php
@@ -13,6 +13,7 @@ $major = array("Manajemen",
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
     <h1>Biodata</h1>
@@ -44,8 +45,9 @@ $major = array("Manajemen",
             <?php
             }?>
     </select><br>
-    <input type="submit" value="okiedokie" name="Sign Up">
+    <input type="submit" value="Sign Up" name="Sign Up">
     <input type="reset" value="Delete" name="reset">
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
--- Latihan3/postPage.php
+++ Latihan3/postPage.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['okiedokie'])){
+if(isset($_POST['Sign In'])){
     $nama = $_POST['name'];
     $email = $_POST['email'];
     $pass = $_POST['pw'];
@@ -17,6 +17,7 @@ if(isset($_POST['okiedokie'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Submit Page</title>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
     <h1>Your Bio</h1>
@@ -34,5 +35,6 @@ if(isset($_POST['okiedokie'])){
     <script>
         swal("Nicely Done!", "Your account has been signed up!", "success");
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
