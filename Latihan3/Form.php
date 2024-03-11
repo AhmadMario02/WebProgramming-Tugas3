45089ddad4165fbcea561f879f943fd989e1b505
Ahmad Mario <riotheassassin@gmail.com>
2024-03-11 17:17:57 +0700

    fixing button

--- Latihan3/Form.php
+++ Latihan3/Form.php
@@ -65,8 +65,8 @@ $major = array("Manajemen",
                 }?>
         </select><br>
     </div>
-    <input type="submit" value="Sign Up" name="btnSubmit" class="btn btn-outline-success btn-lg">
-    <input type="reset" value="Delete" name="reset" class="btn btn-outline-danger btn-lg">
+    <input type="submit" value="Sign Up" name="btnSubmit" class="btn btn-success btn-lg">
+    <input type="reset" value="Delete" name="reset" class="btn btn-danger btn-lg">
     </form>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@523distjsbootstrapbundleminjs" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
